74940a0b58de9b8b65d98a61d10adf992d0a3174
Enzo-sus <121203352+Enzo-sus@users.noreply.github.com>
2023-03-08 15:42:42 -0800

    g

--- pages/index.js
+++ pages/index.js
@@ -75,23 +75,21 @@ export default function Home() {
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
           </a> <
-        Image src = "Images/tinykerbalvirues.jpg"
+        Image src = "/tinykerbalvirues.jpg"
         fill sizes = "(max-width: 768px) 100vw,
         (max - width: 1200 px) 50 vw,
-        33 vw " / >
-        <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
+        33 vw " / > <
+        a href = "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        className = { styles.card } target = "_blank"
+        rel = "noopener noreferrer" >
+        <h2 className={inter.className}>
               Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+            </h2> <
+        p className = { inter.className } >
+        Learn about Next.js in an interactive course with & nbsp; quizzes!
+        <
+        /p> <
+        /a>
 
         <
         a href = "https://en.wikipedia.org/wiki/Snail"
