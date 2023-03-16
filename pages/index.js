738f9d07c6d2158e9400777b79124342ac5450b7
Enzo-sus <121203352+Enzo-sus@users.noreply.github.com>
2023-03-15 17:06:45 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,6 @@ export default function Home() {
         <
         div className = { styles.center } >
         <Image
-            className={styles.logo}
             src="/snail.jpg"
             alt="Next.js Logo"
             width={180}
